444749349f1eeba71d7e47d20c4ea34699dda2bb
Jie Feng <jiefeng.hopkins@gmail.com>
2014-09-24 19:21:15 -0400

    reshaped

--- chrome/collect_images.js
+++ chrome/collect_images.js
@@ -2,7 +2,6 @@
     function getImages(doc) {
         if(!doc || !doc.images || !doc.images.length) return [];
 
-        console.log(doc.images)
         var arr=[], sources=[], img, w, h;
 
         for(var i=0, l=doc.images.length; i < l; i++) {
@@ -10,10 +9,10 @@
             w   = img.offsetWidth;
             h   = img.offsetHeight;
 
-            if(w > 140 && h > 140 && sources.indexOf(img.src) < 0) {
-                sources.push(img.src);
-                arr.push({'src':img.src, 'w':w, 'h':h});
-            }
+//            if(w > 140 && h > 140 && sources.indexOf(img.src) < 0) {
+            sources.push(img.src);
+            arr.push({'src':img.src, 'w':w, 'h':h});
+//            }
         }
         return arr;
     }
